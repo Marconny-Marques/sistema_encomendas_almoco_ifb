56240a039804293b8e5feb52ee0bc67bff29c4df
Marconny Marques <130397974+Marconny-Marques@users.noreply.github.com>
2026-03-21 12:26:05 -0300

    Tudo na raiz definitiva

--- meu_projeto/build.sh
+++ meu_projeto/build.sh
@@ -1,8 +1,5 @@
 #!/usr/bin/env bash
 set -o errexit
-
 pip install -r requirements.txt
-
-# O caminho real baseado na sua foto:
-python projeto_Almoco/manage.py collectstatic --no-input
-python projeto_Almoco/manage.py migrate
+python manage.py collectstatic --no-input
+python manage.py migrate
