a199380778037bc83560e1b458fada9917903afc
Marconny Marques <130397974+Marconny-Marques@users.noreply.github.com>
2026-03-21 12:01:55 -0300

    Ajustando caminho do manage.py no build

--- meu_projeto/build.sh
+++ meu_projeto/build.sh
@@ -2,5 +2,7 @@
 set -o errexit
 
 pip install -r requirements.txt
-python manage.py collectstatic --no-input
-python manage.py migrate
+
+# O segredo está aqui: apontar para a pasta certa
+python projeto_Almoco/manage.py collectstatic --no-input
+python projeto_Almoco/manage.py migrate
