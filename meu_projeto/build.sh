ecd7b3eeaaf0f15a2f1c4c4a01ef9466a7971ed4
Marconny Marques <130397974+Marconny-Marques@users.noreply.github.com>
2026-03-21 12:13:31 -0300

    Manage.py esta na raiz do meu_projeto

--- meu_projeto/build.sh
+++ meu_projeto/build.sh
@@ -3,5 +3,6 @@ set -o errexit
 
 pip install -r requirements.txt
 
-python projeto_Almoco/manage.py collectstatic --no-input
-python projeto_Almoco/manage.py migrate
+# Se o arquivo está na mesma pasta que o build.sh, o comando é esse:
+python manage.py collectstatic --no-input
+python manage.py migrate
