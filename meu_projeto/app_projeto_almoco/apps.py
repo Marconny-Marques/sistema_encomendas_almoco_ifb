fa465924af7987c5623401874a77961d312d8c70
Marconny Marques <130397974+Marconny-Marques@users.noreply.github.com>
2026-04-18 10:56:52 -0300

    Refatoração do esquema de pastas do projeto

--- meu_projeto/app_projeto_almoco/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class AppProjetoAlmocoConfig(AppConfig):
-    name = 'app_projeto_almoco'
--- meu_projeto/app_projeto_almoco/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-
-class Usuario(models.Model):
-    id_usuario = models.AutoField(primary_key= True)
-    nome_usuario = models.TextField(max_length=255)
-    email_usuario = models.TextField(max_length=255)
- 
